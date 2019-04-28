d5ca63898b797c43116ce7ebc61a338aa1623d7e
FFYFYY <2443248443@qq.com>
2019-04-28 19:30:59 +0800

    完善页面

--- ShuWu/novels/urls.py
+++ ShuWu/novels/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('search', views.search_view, name='search'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('register', views.register_view, name='register'),
-    path('novel/<int>', views.novel_view, name='novel')
+    path('search/', views.search_view, name='search'),
+    path('author/', views.author_view, name='author'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('book/<int:book_id>/', views.book_view, name='book'),
+    path('like/<int:book_id>/', views.like_view, name='like')
 ]
--- ShuWu/novels/views.py
+++ ShuWu/novels/views.py
@@ -1,33 +1,91 @@
-from django.shortcuts import render, HttpResponse, redirect, reverse
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.contrib.auth import logout, login, authenticate
 from .models import Books, BookUrls, Likes
 from django.contrib.auth.models import User
 from .form import RegisterForm, LoginForm
-from django.contrib.auth import logout, login, authenticate
+from django.core.cache import cache
+from django.http import JsonResponse
 from django.db.models import Q
 
 
 def index_view(request):
-    books = Books.objects.all()
-    context = {
-        'books': books,
-    }
+    context = {}
+    if request.user.is_authenticated:
+        like_books = Likes.objects.filter(user=request.user)
+        context['books'] = like_books
+
+    hot_search_novels = cache.get('hot_search_novels')
+    if not hot_search_novels:
+        hot_search_novels = Books.objects.all().order_by('-searches')[:30]
+        cache.set('hot_search_novels', hot_search_novels, 3600)
+
+    context['hot_search_novels'] = hot_search_novels
     return render(request, 'index.html', context)
 
 
 def search_view(request):
-    msg = request.POST.get("msg").strip()
-    # sources = Sources.objects.filter(Q(book__icontains=msg))
-    # for i in sources:
-    #     print(i)
-    return HttpResponse(msg)
+    msg = request.GET.get("msg").strip()
+    context = {}
+    context['book_count'] = 0
+    if not msg:
+        return render(request, 'search.html', context)
+    books = Books.objects.filter(
+        Q(book_name__icontains=msg) | Q(author__icontains=msg))
+    book_count = books.count()
+    if book_count < 500:
+        context['books'] = books
+        context['book_count'] = book_count
+    else:
+        context['book_count'] = 0
+    return render(request, 'search.html', context)
 
 
-def novel_view(request):
-    pass
+def author_view(request):
+    msg = request.GET.get("msg").strip()
+    context = {}
+    if not msg:
+        context['book_count'] = 0
+        return render(request, 'search.html', context)
+    books = Books.objects.filter(author=msg)
+    book_count = books.count()
+    context['books'] = books
+    context['book_count'] = book_count
+    return render(request, 'search.html', context)
 
 
-def like_view(request):
-    pass
+def book_view(request, book_id):
+    book = get_object_or_404(Books, id=book_id)
+    book_urls = BookUrls.objects.filter(book=book_id)
+    context = {}
+    if request.user.is_authenticated:
+        is_like = Likes.objects.filter(book=book, user=request.user)
+        if is_like:
+            context['like'] = "取消收藏"
+        else:
+            context['like'] = "加入书架"
+    context['book'] = book
+    context['book_urls'] = book_urls
+    book.searches += 1
+    book.save()
+    return render(request, 'book.html', context)
+
+
+def like_view(request, book_id):
+    data = {}
+    if request.user.is_authenticated:
+        book = get_object_or_404(Books, id=book_id)
+        is_like = Likes.objects.filter(book=book, user=request.user)
+        if is_like:
+            is_like.delete()
+            data['like'] = False
+        else:
+            like = Likes(book=book, user=request.user)
+            like.save()
+            data['like'] = True
+        data['status'] = 'SUCCESS'
+    else:
+        data['status'] = 'ERROR'
+    return JsonResponse(data)
 
 
 def logout_view(request):
